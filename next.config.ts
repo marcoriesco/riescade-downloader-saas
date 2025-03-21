ec932573a37dff7ac65debe5ac5844b213de31e9
SHARKDEV <sharkdev.com.br@gmail.com>
2025-03-21 17:14:28 -0300

    updates

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   allowedDevOrigins: [
-    "http://localhost:3000",
-    "https://e892-2804-7f0-aa1a-d0e9-8c3e-d058-ad80-ea25.ngrok-free.app",
+    "localhost",
+    "e892-2804-7f0-aa1a-d0e9-8c3e-d058-ad80-ea25.ngrok-free.app",
   ],
 };
 
