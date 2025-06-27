836602a287007adfd8ed50b59592918197b4a87c
SHARKDEV <sharkdev.com.br@gmail.com>
2025-06-26 23:43:30 -0300

    Footer zindex

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const robotoCondensed = Roboto_Condensed({
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white py-12 border-t border-gray-800">
+    <footer className="relative bg-black text-white py-12 border-t border-gray-800 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="md:flex md:justify-between">
           <div className="mb-8 md:mb-0">
